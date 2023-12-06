b6a982ebedf5244fa03a8dcfd57e809e6047cb03
ml27 <120706965@umail.ucc.ie>
2023-12-06 21:19:54 +0000

    test: Add additional tests to ColourTableTest
    Extended the ColourTableTest class with additional tests to enhance code coverage and ensure the correctness of the ColourTable implementation. New tests cover scenarios such as getting the palette after adding colors, adding colors beyond capacity, and getting the palette when no colors have been added. These tests contribute to a more comprehensive test suite for the ColourTable class.

--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -39,8 +39,10 @@ class ColourTableTest {
         ColourTable colourTable = new ColourTable(4);
 
         // Attempt to add an invalid RGB color (negative value)
-        assertThrows(IllegalArgumentException.class, () -> colourTable.add(-1));
-    }
+        assertThrows(IllegalArgumentException.class, () -> {
+            // Use a negative RGB value, which should throw an exception
+            colourTable.add(-1);
+        });}
 
     @Test
     void testExceedCapacity() {
@@ -48,6 +50,38 @@ class ColourTableTest {
         colourTable.add(0xFF0000); // Red
         colourTable.add(0x00FF00); // Green
 
-        assertThrows(IllegalStateException.class, () -> colourTable.add(0x0000FF)); // Blue
+        // Blue
+    }
+    @Test
+    void testGetPaletteAfterAddingColors() {
+        ColourTable colourTable = new ColourTable(3);
+        colourTable.add(0xFF0000); // Red
+        colourTable.add(0x00FF00); // Green
+        colourTable.add(0x0000FF); // Blue
+
+        List<Integer> palette = colourTable.getPalette();
+        assertEquals(3, palette.size());
+        assertTrue(palette.contains(0xFF0000));
+        assertTrue(palette.contains(0x00FF00));
+        assertTrue(palette.contains(0x0000FF));
+    }
+
+    @Test
+    void testAddColorsBeyondCapacity() {
+        ColourTable colourTable = new ColourTable(2);
+        colourTable.add(0xFF0000); // Red
+        colourTable.add(0x00FF00); // Green
+
+        assertThrows(IllegalStateException.class, () -> {
+            // Attempt to add another color beyond the capacity
+            colourTable.add(0x0000FF); // Blue
+        });
+    }
+
+    @Test
+    void testGetPaletteWithNoColorsAdded() {
+        ColourTable colourTable = new ColourTable(5);
+        List<Integer> palette = colourTable.getPalette();
+        assertTrue(palette.isEmpty());
     }
 }
